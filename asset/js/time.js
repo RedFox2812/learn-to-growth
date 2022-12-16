e46a728f955a7fb02ab03ee3718497fc0f7a7630
RedFox2812 <alan.hany123@gmail.com>
2022-12-16 15:47:08 +0700

    up time 3

--- asset/js/time.js
+++ asset/js/time.js
@@ -4,6 +4,10 @@ let timeSecondCurr = 0;
 let timeMinuteCurr = 0;
 let timeOfSelectionS = 0;
 let timeOfSelectionM = 0;
+let valueTime = 0;
+let index = 0;
+let value = "break";
+let index3 = 0;
 btnStartTimeCount.addEventListener("click", handleStartOrStop);
 function handleStartOrStop(e){
     // change text 
@@ -34,6 +38,11 @@ function countTimeft(){
         clearTimeout(count)
         return;
     }
+    if(value == "pause"){
+        clearTimeout(count);
+        countTimes();
+        return;
+    }
     const timeMinute = document.querySelector("#minute");
     const timeSecond = document.querySelector("#second");
     if(Number(timeSecond.innerHTML) === 0){
@@ -52,36 +61,64 @@ function countTimeft(){
         timeSecond.innerHTML =timeSecondCurr;
     }
     console.log(timeSecondCurr)
+    console.log(value) 
+    
     if(timeSecond.innerHTML <= 0 && timeMinute.innerHTML <=0){
-        textTime.innerHTML = "Pause"; 
-        timeSecond.innerHTML = "03";
-        timeMinute.innerHTML = "00";
-        const selection = document.querySelector(".time__selection--active");
-
-        if(selection.matches(".time__selection--end")){
-            selection.classList.remove("time__selection--active");
-            const selectionBegin = document.querySelector(".time__selection--begin");
-            selectionBegin.classList.add("time__selection--active");
-        } else {
+       
+        if(value=="break"){
+            textTime.style = "display:none"; 
+            timeSecond.innerHTML = "03";
+            timeMinute.innerHTML = "00";
+            value = "run";    
+        }
+        else if(value =="run"){
+            textTime.style = "display: block";
             changeSelection();
+            if(index == 0){
+                timeSecond.innerHTML = "00";
+                timeMinute.innerHTML = "20";
+            }
+            if(index == 1){
+                timeSecond.innerHTML = "00";
+                timeMinute.innerHTML = "05";
+            }
+            if(index == 2){
+                timeSecond.innerHTML = "00";
+                timeMinute.innerHTML = "15";
+            }
         }
     }
     var count = setTimeout(countTimeft, 1000);
 }
 function changeSelection(){
-    const selection = document.querySelector(".time__selection--active");
-    if(selection.matches("#pomodoro")){
-        timeOfSelectionM = 20;
-        timeOfSelectionS = 00; 
-        const timeMinute = document.querySelector("#minute");
-        const timeSecond = document.querySelector("#second");
-        timeSecond.innerHTML = timeOfSelectionS;
-        timeMinute.innerHTML = timeOfSelectionM;
-    }
-    if(selection.matches(".time__selection")){
-        selection.classList.remove("time__selection--active");
-        selection.nextElementSibling.classList.add("time__selection--active");
-    }
+    const selections = document.querySelectorAll(".time__selection");
+    index++;
+    value = "break";
+    if(index < 3){
+        [...selections].forEach(item => item.classList.remove("time__selection--active"));
+        [...selections][index].classList.add("time__selection--active");
+        // set pomodoro time
+    };
+    if(index == 3)
+    {   
+        index = 0;
+        value = "pause";
+        [...selections].forEach(item => item.classList.remove("time__selection--active"));
+        [...selections][index].classList.add("time__selection--active");
+    };
+};
+function countTimes(){
+    const circles = document.querySelectorAll(".circle");
+    if(index3 < 4){
+        [...circles][index3].classList.add("circle--active");
+        index3++;
+        // set pomodoro time
+    };
+    if(index3 == 4)
+    {   
+        index3= 0;
+        [...circles].forEach(item => item.classList.remove("circle--active"));
+    };
 }
 // 2. selections 
 // 3. công thức vòng lặp
